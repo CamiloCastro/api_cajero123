08a09d628043fec8a7a023e56e22ea735daa1538
Camilo <jccastrop@unal.edu.co>
2020-12-10 08:37:56 -0500

    Adición de CORS

--- db/user_db.py
+++ db/user_db.py
@@ -11,7 +11,7 @@ database_users = Dict[str, UserInDB]
 database_users = {
     "camilo24": UserInDB(**{"username":"camilo24",
                             "password":"root",
-                            "balance":12000}),
+                            "balance":20000}),
     "andres18": UserInDB(**{"username":"andres18",
                             "password":"hola",
                             "balance":34000}),
